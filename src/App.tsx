21b8b2af6e5609417e258f76e1fb7cd2f21e8090
unknown <jorgemendoza2002@gmail.com>
2023-01-18 16:10:05 -0600

    install axios to use with swr to easily grab and type fetch results, display result of fetch api in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import fetchAdvice, { AdviceResponse } from './api/fetch-advice';
 import AdviceCard from './components/AdviceCard';
+import useSWR from 'swr';
 
 function App() {
+  const { data, error, isLoading } = useSWR<AdviceResponse, Error>(
+    'advice',
+    fetchAdvice
+  );
   return (
     <main>
       {/* advice card container */}
+      {data && <p>{data.slip.advice}</p>}
       <div>
         <AdviceCard />
         <AdviceCard />
