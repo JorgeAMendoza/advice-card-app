201c4e7d5b892d8dd1718820353c4c700366c5cf
unknown <jorgemendoza2002@gmail.com>
2023-01-27 10:37:33 -0600

    advice data can be fetched using swr and is applied to state to use for the front and back of the cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,44 @@ import './App.css';
 import fetchAdvice, { AdviceResponse } from './api/fetch-advice';
 import AdviceCard from './components/AdviceCard';
 import useSWR from 'swr';
+import type { AdviceCardContent } from './types/advice-card';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // front card State
+  // backcard state
+  const [frontCard, setFrontCard] = useState<AdviceCardContent | null>(null);
+  const [backCard, setBackCard] = useState<AdviceCardContent | null>(null);
+  const [flipToBack, setflipToBack] = useState(true);
   const { data, error, isLoading } = useSWR<AdviceResponse, Error>(
     'advice',
     fetchAdvice
   );
+
+  useEffect(() => {
+    if (isLoading) return;
+    if (!data) return;
+
+    if (!frontCard || flipToBack) {
+      setFrontCard({ adviceId: data.slip.id, adviceText: data.slip.advice });
+      setflipToBack(false);
+      console.log('showing front');
+      return;
+    } else {
+      setBackCard({ adviceId: data.slip.id, adviceText: data.slip.advice });
+      setflipToBack(true);
+      return;
+    }
+  }, [data]);
+
   return (
     <main>
       {/* advice card container */}
-      {data && <p>{data.slip.advice}</p>}
       <div>
         <AdviceCard />
         <AdviceCard />
       </div>
+      {error && <p>{error.message}</p>}
     </main>
   );
 }
